f3a80f189138263f078a4b5b37bf48701bbd0cf5
Poytr1 <pooytr1@gmail.com>
2024-03-08 15:32:46 +0800

    add tracer server

--- crates/sui-replay/src/converter.rs
+++ crates/sui-replay/src/converter.rs
@@ -1,15 +1,16 @@
 use anyhow::{anyhow, Result};
-use move_core_types::{identifier::Identifier, runtime_value::MoveValue};
+use move_core_types::identifier::Identifier;
 use serde_json::{Map, Value};
+use move_core_types::annotated_value as A;
 
-pub fn move_value_to_json(val: MoveValue) -> Value {
+pub fn move_value_to_json(val: A::MoveValue) -> Value {
     match val {
-        MoveValue::U8(n) => serde_json::to_value(n).unwrap(),
-        MoveValue::U64(n) => serde_json::to_value(n.to_string()).unwrap(),
-        MoveValue::U128(n) => serde_json::to_value(n.to_string()).unwrap(),
-        MoveValue::Bool(b) => serde_json::to_value(b).unwrap(),
-        MoveValue::Address(add) => serde_json::to_value(add).unwrap(),
-        MoveValue::Vector(vals) => {
+        A::MoveValue::U8(n) => serde_json::to_value(n).unwrap(),
+        A::MoveValue::U64(n) => serde_json::to_value(n.to_string()).unwrap(),
+        A::MoveValue::U128(n) => serde_json::to_value(n.to_string()).unwrap(),
+        A::MoveValue::Bool(b) => serde_json::to_value(b).unwrap(),
+        A::MoveValue::Address(add) => serde_json::to_value(add).unwrap(),
+        A::MoveValue::Vector(vals) => {
             // If this is a vector<u8>, convert it to hex string
             if is_non_empty_vector_u8(&vals) {
                 let bytes = vec_to_vec_u8(vals).unwrap();
@@ -18,21 +19,15 @@ pub fn move_value_to_json(val: MoveValue) -> Value {
                 Value::Array(vals.into_iter().map(|v| move_value_to_json(v)).collect())
             }
         }
-        MoveValue::Struct(move_struct) => Value::Array(
-            move_struct
-                .0
-                .into_iter()
-                .map(|v| move_value_to_json(v))
-                .collect(),
-        ),
-        MoveValue::Signer(add) => serde_json::to_value(add).unwrap(),
-        MoveValue::U16(n) => serde_json::to_value(n).unwrap(),
-        MoveValue::U32(n) => serde_json::to_value(n).unwrap(),
-        MoveValue::U256(n) => serde_json::to_value(n.to_string()).unwrap(),
+        A::MoveValue::Struct(move_struct) => struct_fields_to_json(move_struct.fields),
+        A::MoveValue::Signer(add) => serde_json::to_value(add).unwrap(),
+        A::MoveValue::U16(n) => serde_json::to_value(n).unwrap(),
+        A::MoveValue::U32(n) => serde_json::to_value(n).unwrap(),
+        A::MoveValue::U256(n) => serde_json::to_value(n.to_string()).unwrap(),
     }
 }
 
-fn struct_fields_to_json(fields: Vec<(Identifier, MoveValue)>) -> Value {
+fn struct_fields_to_json(fields: Vec<(Identifier, A::MoveValue)>) -> Value {
     let mut iter = fields.into_iter();
     let mut map = Map::new();
     while let Some((field_name, field_value)) = iter.next() {
@@ -41,22 +36,22 @@ fn struct_fields_to_json(fields: Vec<(Identifier, MoveValue)>) -> Value {
     Value::Object(map)
 }
 
-fn is_non_empty_vector_u8(vec: &Vec<MoveValue>) -> bool {
+fn is_non_empty_vector_u8(vec: &Vec<A::MoveValue>) -> bool {
     if vec.is_empty() {
         false
     } else {
-        matches!(vec.last().unwrap(), MoveValue::U8(_))
+        matches!(vec.last().unwrap(), A::MoveValue::U8(_))
     }
 }
 
 /// Converts the `Vec<MoveValue>` to a `Vec<u8>` if the inner `MoveValue` is a `MoveValue::U8`,
 /// or returns an error otherwise.
-fn vec_to_vec_u8(vec: Vec<MoveValue>) -> Result<Vec<u8>> {
+fn vec_to_vec_u8(vec: Vec<A::MoveValue>) -> Result<Vec<u8>> {
     let mut vec_u8 = Vec::with_capacity(vec.len());
 
     for byte in vec {
         match byte {
-            MoveValue::U8(u8) => {
+            A::MoveValue::U8(u8) => {
                 vec_u8.push(u8);
             }
             _ => {
--- external-crates/move/crates/move-core-types/src/call_trace.rs
+++ external-crates/move/crates/move-core-types/src/call_trace.rs
@@ -1,5 +1,6 @@
 use crate::runtime_value::MoveValue;
 use std::collections::HashSet;
+use crate::annotated_value as A;
 const CALL_STACK_SIZE_LIMIT: usize = 1024;
 
 /// A call trace
@@ -11,8 +12,8 @@ pub struct InternalCallTrace {
     pub from_module_id: String,
     pub module_id: String,
     pub func_name: String,
-    pub inputs: Vec<MoveValue>,
-    pub outputs: Vec<MoveValue>,
+    pub inputs: Vec<A::MoveValue>,
+    pub outputs: Vec<A::MoveValue>,
     pub type_args: Vec<String>,
     pub sub_traces: CallTraces,
     pub fdef_idx: u16,
@@ -55,7 +56,7 @@ impl CallTraces {
         self.0.pop()
     }
 
-    pub fn set_outputs(&mut self, outputs: Vec<MoveValue>) {
+    pub fn set_outputs(&mut self, outputs: Vec<A::MoveValue>) {
         let length = self.0.len();
         self.0[length - 1].outputs = outputs
     }
