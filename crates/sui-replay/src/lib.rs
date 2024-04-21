62f3d26e1c42b8a28dd7b78063e3cd84f77b7c89
Poytr1 <pooytr1@gmail.com>
2024-04-21 10:23:03 +0800

    support type param resolvation

--- crates/sui-replay/src/lib.rs
+++ crates/sui-replay/src/lib.rs
@@ -605,7 +605,7 @@ pub async fn execute_replay_command(
                 tx_digest,
                 safety,
                 use_authority,
-                Some(2),
+                Some(-1),
                 None,
                 None,
                 true,
